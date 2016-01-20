b2eb14b81f4545beb63adf377b753c35f8ca25f5
adam <agarci11@BNUTJHRC-75K8D.tufts.ad.tufts.edu>
2016-01-20 12:49:50 -0500

    added warmup and commute calculators, removed column layout

--- calc.js
+++ calc.js
@@ -9,8 +9,9 @@ function showForm (formID) {
 function ftMilesCalc () {
 	calc = eval($(" #numFtMiles ").val());
 	var dist = eval($(" #distFtMiles ").val());
+	var laps = eval($(" #lapsFtMiles ").val());
 	$(" #result ").removeClass("hide");
-	displayMiles( calc * dist );
+	displayMiles( calc * dist * laps );
 	return false;
 }
 
@@ -19,6 +20,24 @@ function ftPacerCalc () {
 	var lap = eval($( "#lapFtPacer" ).val());
 	var len = eval($( "#lenFtPacer" ).val());
 	calc = Math.floor(num * lap * len / 1600);
+	displayMiles( calc );
+	return false;
+}
+
+function warmupCalc () {
+	var num = eval($( "#numWarmup" ).val());
+	var dur = eval($( "#durWarmup" ).val());
+	var pace = 1 / eval($( "#paceWarmup" ).val());
+	calc = Math.floor(num * dur * pace);
+	displayMiles( calc );
+	return false;
+}
+
+function commuteCalc () {
+	var num = eval($( "#numCommute" ).val());
+	var dist = eval($( "#distCommute" ).val());
+	var dir = eval($( "#dirCommute" ).val());
+	calc = Math.floor(num * dist * dir);
 	displayMiles( calc );
 	return false;
 }
