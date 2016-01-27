0400753a8bd50d31256db9be5fdfc2b2487daf89
adam <agarci11@BNUTJHRC-75K8D.tufts.ad.tufts.edu>
2016-01-27 13:34:51 -0500

    radically simplified javascript

--- calc.js
+++ calc.js
@@ -1,83 +1,25 @@
-var calc = 0;
-
-function showForm (formID) {
-    $(formID).toggleClass("hide");
-    $(".activities").toggleClass("hide");
-}
-
-function ftMilesCalc () {
-    calc = eval($(" #numFtMiles ").val());
-    var dist = eval($(" #distFtMiles ").val());
-    var laps = eval($(" #lapsFtMiles ").val());
-    $(" #result ").removeClass("hide");
-    return displayMiles( calc * dist * laps );
-}
-
-function ftPacerCalc () {
-    var num = eval($( "#numFtPacer" ).val());
-    var lap = eval($( "#lapFtPacer" ).val());
-    var len = eval($( "#lenFtPacer" ).val());
-    calc = Math.floor(num * lap * len / 1600);
-    return displayMiles( calc );
-}
-
-function warmupCalc () {
-    var num = eval($( "#numWarmup" ).val());
-    var dur = eval($( "#durWarmup" ).val());
-    var pace = 1 / eval($( "#paceWarmup" ).val());
-    calc = Math.floor(num * dur * pace);
-    return displayMiles( calc );
-}
-
-function commuteCalc () {
-    var num = eval($( "#numCommute" ).val());
-    var dist = eval($( "#distCommute" ).val());
-    var dir = eval($( "#dirCommute" ).val());
-    calc = Math.floor(num * dist * dir);
-    return displayMiles( calc );
-}
-
-function funrunCalc () {
-    var num = eval($( "#numFunrun" ).val());
-    var dist = eval($( "#distFunrun" ).val());
-    var unit = $( "#unitFunrun" ).val();
-    var weight = 1;
-    if (unit == "km") {
-        weight = 1.60934;
+function displayMiles (type) {
+    var miles;
+    var num = eval($( "#numStudents" ).val());
+    var grp = eval($( "#numGroups" ).val());
+    if (type == "time") {
+        var dur = eval($( "#duration" ).val());
+        var pace = eval($( "#pace" ).val());
+        miles = Math.floor( num * grp * dur / pace)
+    } else if (type == "dist") {
+        var dist = eval($( "#distance" ).val());
+        var weight = eval($( "#units" ).val());
+        miles = Math.floor( num * grp * dist / weight );
+    } else if (type == "pedom") {
+        var dist = eval($( "#distance" ).val());
+        miles = Math.floor( num * grp * dist / 2500 )
     }
-    calc = Math.floor(num * dist / weight);
-    return displayMiles( calc );
-}
 
-function walkjogCalc () {
-    var num = eval($( "#numWalkjog" ).val());
-    var dur = eval($( "#durWalkjog" ).val());
-    var pace = 1 / eval($( "#paceWalkjog" ).val());
-    calc = Math.floor(num * dur * pace);
-    return displayMiles( calc );
-}
-
-function pedomCalc () {
-    var num = eval($( "#numPedom" ).val());
-    var dist = eval($( "#distPedom" ).val());
-    var tall = eval($( "#tallPedom" ).val());
-    if (tall >= 1)  {
-        tall = tall / 100;
-    }
-    tallMiles = tall * num * dist / 2500;
-    shortSteps = (1 - tall) * num * dist / 2000;
-    calc = Math.floor(tallMiles) + Math.floor(shortSteps);
-    // tabling tall/short proportion for now
-    calc = Math.floor(num * dist / 2500);
-    return displayMiles(calc);
-
-}
-
-function displayMiles (miles) {
     $(" #result ").removeClass("hide");
     $(" #miles ").text( miles );
     $('html, body').animate({
         scrollTop: $("#result").offset().top
     }, 2000);
+
     return false;
 }
