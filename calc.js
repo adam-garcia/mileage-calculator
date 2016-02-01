cdfc485197ee968a70746385b7fd43b8a39eb888
adam <agarci11@BNUTJHRC-75K8D.tufts.ad.tufts.edu>
2016-02-01 16:47:40 -0500

    wording changes from dan

--- calc.js
+++ calc.js
@@ -1,21 +1,34 @@
+var congrats = ["BOOM!",
+                "Way to go!",
+                "Your kids are crushing it!"];
+
+var directns = ["Click the button below to post those miles to your dashboard."];
+
 function displayMiles (type) {
     var miles;
-    var num = eval($( "#numStudents" ).val());
-    var grp = eval($( "#numGroups" ).val());
     if (type == "time") {
-        var dur = eval($( "#duration" ).val());
-        var pace = eval($( "#pace" ).val());
+        var num = eval($( "#numStudentsTime" ).val());
+        var grp = eval($( "#numGroupsTime" ).val());
+        var dur = eval($( "#durationTime" ).val());
+        var pace = eval($( "#paceTime" ).val());
         miles = Math.floor( num * grp * dur / pace)
     } else if (type == "dist") {
-        var dist = eval($( "#distance" ).val());
-        var weight = eval($( "#units" ).val());
+        var num = eval($( "#numStudentsDist" ).val());
+        var grp = eval($( "#numGroupsDist" ).val());
+        var dist = eval($( "#distanceDist" ).val());
+        var weight = eval($( "#unitsDist" ).val());
         miles = Math.floor( num * grp * dist / weight );
     } else if (type == "pedom") {
-        var dist = eval($( "#distance" ).val());
+        var num = eval($( "#numStudentsPedom" ).val());
+        var grp = eval($( "#numGroupsPedom" ).val());
+        var dist = eval($( "#distancePedom" ).val());
         miles = Math.floor( num * grp * dist / 2500 )
     }
     $(" #result ").removeClass("hide");
     $(" #miles ").text( miles );
+    var message = congrats[Math.floor(Math.random()*congrats.length)] + 
+                  "<br>" + directns[Math.floor(Math.random()*directns.length)]
+    $(" #message ").html( message );
     $('html, body').animate({scrollTop: $("#result").offset().top}, 1500);
     return false;
 }
